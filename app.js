f5fbafebe011027032bb8a6ea2962dc7c05d4ec9
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-29 01:19:19 +1000

    Add parallaxes

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var sidenavElems = document.querySelectorAll('.sidenav');
     var sidenavInstance = M.Sidenav.init(sidenavElems);
     var materialboxElems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(materialboxElems);
+    var materialboxInstances = M.Materialbox.init(materialboxElems);
+    var parallaxElems = document.querySelectorAll('.parallax');
+    var parallaxInstances = M.Parallax.init(parallaxElems);
   });
